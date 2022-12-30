9ead40c4ceaf77e9cd69d23ff5c5ff78a3c255f8
Thomas Thirion <thirionthomas68100@gmail.com>
2022-12-30 14:32:15 +0100

    séparer front et back

--- exemple.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-require("./models/dbConfig");
-
-app.listen(5500, () => console.log("server lancé: 5500"));
-
-//react-scripts start
--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ export const Form = () => {
     <>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" action="/form" method="post">
+          <form className="space-y-6" action="/" method="post">
             <div>
               <label
                 for="email"
